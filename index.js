ce121f15a01230a9e5fd25cfac07ca3bab25b635
Rj Saini <rsaini88@gmail.com>
2022-12-04 20:17:07 -0800

    App working up to create HTML with specified roles, needs styling

--- index.js
+++ index.js
@@ -1,8 +1,126 @@
-
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-const fs = require('fs');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
+const employees = [];
+
+function newEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the employee?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the email of the employee?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the id of the employee?",
+      },
+      {
+        type: "list",
+        name: "position",
+        message: "What position is this employee",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+    ])
+    .then(({ name, email, id, position }) => {
+      switch (position) {
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the office number?",
+              },
+            ])
+            .then(({ officeNumber }) => {
+              employees.push(new Manager(name, email, id, officeNumber));
+              addAnother();
+            });
+          break;
+
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "schoolName",
+                message: "What is the name of of the school they attended?",
+              },
+            ])
+            .then(({ schoolName }) => {
+              employees.push(new Intern(name, email, id, schoolName));
+              addAnother();
+            });
+          break;
+
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "gitHub",
+                message: "What is thier github username?",
+              },
+            ])
+            .then(({ github }) => {
+              employees.push(new Engineer(name, email, id, github));
+              addAnother();
+            });
+          break;
+        default:
+      }
+    });
+}
+
+function addAnother() {
+  return inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "addMore",
+        message: "Would you like to create another employee?",
+      },
+    ])
+    .then(({ addMore }) => {
+      if (addMore) newEmployee();
+      // else console.log(employees)
+      else {
+        renderHTML();
+      }
+    });
+}
+
+newEmployee();
+
+function renderHTML() {
+  fs.writeFileSync(
+    "./index.HTML", //HTML
+    `
+     <ul>
+     ${employees.map((employee) => `
+     <li>${employee.getName()}</li>
+     <li>${employee.getId()}</li>
+     <li>${employee.getEmail()}</li>    
+     <li>${employee.getRole()}</li>
+     
+     `)}
+     
+     </ul>
+
+    `
+  )
+}
+// ${employees.map((employee) => `<li> ${employee.getEmail()}</li>`)}
+// ${employees.map((employee) => `<li>${employee.getID()}</li>,`)}
+// ${employees.map((employee) => `<li>${employee.getRole()}</li>,`)}
